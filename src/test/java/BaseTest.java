2115edcdf9710f024d9bcece9570748ea78b27a9
Alex Bulgakov <abulgakoff@Alexs-MacBook-Pro.local>
2019-09-01 21:34:06 +0200

    Moved preparation/cleanup of service to BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,14 +1,35 @@
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
+import DriverManager.ChromeDriverManager;
+import DriverManager.DriverManager;
+import DriverManager.DriverManagerFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class BaseTest {
     String browser;
+    DriverManager driverManager;
+    WebDriver driver;
 
     @BeforeTest
     @Parameters({"browser"})
     public void beforeTest(@Optional("CHROME") String browser) {
        this.browser = browser;
+    }
+
+    @BeforeClass
+    public void beforeTest() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        driverManager = DriverManagerFactory.getManager(ChromeDriverManager.class);
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver = driverManager.getDriver();
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driverManager.quitDriver();
     }
 }
 
--- src/test/java/FactoryPatternTest.java
+++ src/test/java/FactoryPatternTest.java
@@ -1,35 +1,10 @@
 import Pages.GooglePage;
 import Pages.YahooPage;
-import DriverManager.ChromeDriverManager;
-import DriverManager.DriverManager;
-import DriverManager.DriverManagerFactory;
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.lang.reflect.InvocationTargetException;
-
 public class FactoryPatternTest extends BaseTest {
-    DriverManager driverManager;
-    WebDriver driver;
 
-    @BeforeClass
-    public void beforeTest() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        driverManager = DriverManagerFactory.getManager(ChromeDriverManager.class);
-    }
-
-    @BeforeMethod
-    public void beforeMethod() {
-        driver = driverManager.getDriver();
-    }
-
-    @AfterMethod
-    public void afterMethod() {
-        driverManager.quitDriver();
-    }
 
     @Test
     public void launchGooglePageTest() {
